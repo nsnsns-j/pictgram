c59149aa6dbd7d0a21f658e501ae9140c2d44168
kato <mns0904.pj@gmail.com>
2022-07-31 10:10:14 +0900

    add Favorite #5

--- src/main/java/com/example/pictgram/controller/SessionsController.java
+++ src/main/java/com/example/pictgram/controller/SessionsController.java
@@ -1,6 +1,10 @@
 package com.example.pictgram.controller;
 
+import java.util.Locale;
+
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,28 +12,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SessionsController {
 
-    @GetMapping(path = "/login")
-    public String index() {
-        return "sessions/new";
-    }
+	@Autowired
+	private MessageSource messageSource;
 
-    @GetMapping(path = "/login-failure")
-    public String loginFailure(Model model) {
-        model.addAttribute("hasMessage", true);
-        model.addAttribute("class", "alert-danger");
-        model.addAttribute("message", "Emailまたはパスワードに誤りがあります。");
+	@GetMapping(path = "/login")
+	public String index() {
+		return "sessions/new";
+	}
 
-        return "sessions/new";
-    }
+	@GetMapping(path = "/login-failure")
+	public String loginFailure(Model model, Locale locale) {
+		model.addAttribute("hasMessage", true);
+		model.addAttribute("class", "alert-danger");
+		model.addAttribute("message", messageSource.getMessage("sessions.loginFailure.flash", new String[] {}, locale));
 
-    @GetMapping(path = "/logout-complete")
-    public String logoutComplete(Model model) {
-        model.addAttribute("hasMessage", true);
-        model.addAttribute("class", "alert-info");
-        model.addAttribute("message", "ログアウトしました。");
+		return "sessions/new";
+	}
 
-        return "layouts/complete";
-    }
+	@GetMapping(path = "/logout-complete")
+	public String logoutComplete(Model model, Locale locale) {
+		model.addAttribute("hasMessage", true);
+		model.addAttribute("class", "alert-info");
+		model.addAttribute("message",
+				messageSource.getMessage("sessions.logoutComplete.flash", new String[] {}, locale));
+
+		return "layouts/complete";
+	}
 }
 
 // /login の呼び出しで index メソッドはログイン画面を表示します。
