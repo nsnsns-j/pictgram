a6440891c66f974488de443b5e57080406f3accd
kato <mns0904.pj@gmail.com>
2022-08-07 08:58:41 +0900

    add S3

--- src/main/java/com/example/pictgram/service/S3Wrapper.java
+++ src/main/java/com/example/pictgram/service/S3Wrapper.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,17 +12,24 @@ import java.util.List;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 @Service
@@ -60,6 +68,21 @@ public class S3Wrapper {
 				});
 
 		return putObjectResults;
+	}
+
+	public ResponseEntity<byte[]> download(String key) throws IOException {
+		GetObjectRequest getObjectRequest = new GetObjectRequest(awsBucket, key);
+		S3Object s3Object = s3Client.getObject(getObjectRequest);
+		S3ObjectInputStream objectInputStream = s3Object.getObjectContent();
+		byte[] bytes = IOUtils.toByteArray(objectInputStream);
+		String fileName = URLEncoder.encode(key, "UTF-8").replaceAll("\\+", "%20");
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+		httpHeaders.setContentLength(bytes.length);
+		httpHeaders.setContentDispositionFormData("attachment", fileName);
+
+		return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
+
 	}
 
 	public List<S3ObjectSummary> list() {
